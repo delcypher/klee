delcypher/klee

commit d310cc0c85ce823c321ef5c04abb7c02027b1146

SMTLIBv2 comments are now not printed when printing in non-human-readable mode.

lib/Expr/ExprSMTLIBPrinter.cpp
@@ -341,7 +341,7 @@ namespace klee
 		//Scan the query too
 		scan(query.expr);
 
-		printNotice();
+		if(humanReadable) printNotice();
 		printOptions();
 		printSetLogic();
 		printArrayDeclarations();
@@ -366,7 +366,8 @@ namespace klee
 	void ExprSMTLIBPrinter::printArrayDeclarations()
 	{
 		//Assume scan() has been called
-		o << "; Array declarations" << endl;
+		if(humanReadable)
+			o << "; Array declarations" << endl;
 
 		//declare arrays
 		for(set<const Array*>::iterator it = usedArrays.begin(); it != usedArrays.end(); it++)
@@ -379,7 +380,8 @@ namespace klee
 		//Set array values for constant values
 		if(haveConstantArray)
 		{
-			o << "; Constant Array Definitions" << endl;
+			if(humanReadable)
+				o << "; Constant Array Definitions" << endl;
 
 			const Array* array;
 
@@ -423,7 +425,9 @@ namespace klee
 
 	void ExprSMTLIBPrinter::printConstraints()
 	{
-		o << "; Constraints" << endl;
+		if(humanReadable)
+			o << "; Constraints" << endl;
+
 		//Generate assert statements for each constraint
 		for(ConstraintManager::const_iterator i= query.constraints.begin(); i != query.constraints.end(); i++)
 		{
@@ -529,8 +533,7 @@ namespace klee
 
 	void ExprSMTLIBPrinter::printNotice()
 	{
-		o << "; This file conforms to SMTLIBv2 and was generated by KLEE with " <<
-				(humanReadable?"":"no ") << "indentation" << endl;
+		o << "; This file conforms to SMTLIBv2 and was generated by KLEE" << endl;
 	}
 
 	void ExprSMTLIBPrinter::setHumanReadable(bool hr)
@@ -549,7 +552,10 @@ namespace klee
 
 	void ExprSMTLIBPrinter::printQuery()
 	{
-		p << "; Query from solver turned into an assert"; p.breakLineI();
+		if(humanReadable)
+			p << "; Query from solver turned into an assert";
+
+		p.breakLineI();
 		p.pushIndent();
 		p << "(assert";
 		p.pushIndent();

include/klee/util/ExprSMTLIBPrinter.h
@@ -87,8 +87,9 @@ namespace klee {
 			///Sets how readable the printed SMTLIBv2 commands are.
 			/// \param hr If set to true the printed commands are made more human readable.
 			///
-			/// The printed commands are made human readable by indenting and line breaking
-			/// appropriately.
+			/// The printed commands are made human readable by...
+			/// - Indenting and line breaking.
+			/// - Adding comments
 			void setHumanReadable(bool hr);
 
 			///Set SMTLIB options.

commit e1e84b150f5a6e28e06fef148964d21d51a907af

Fixed bug introduced in commit d310cc0c85ce823c321ef5c04abb7c02027b1146 . A call to printSeperator() was accidently removed.

lib/Expr/ExprSMTLIBPrinter.cpp
@@ -552,7 +552,10 @@ namespace klee
 	void ExprSMTLIBPrinter::printQuery()
 	{
 		if(humanReadable)
+		{
 			p << "; Query from solver turned into an assert";
+			p.breakLineI();
+		}
 
 		p.pushIndent();
 		p << "(assert";