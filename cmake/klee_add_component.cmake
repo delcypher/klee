974b2684623d47e0b570346b7b5c0cabbaad0c16
Dan Liew <daniel.liew@imperial.ac.uk>
2016-11-02 10:49:41 +0000

    Fix warnings when building `klee-replay` about irrelevant flags (e.g. `-fno-rtti`) when building C code.

--- cmake/klee_add_component.cmake
+++ cmake/klee_add_component.cmake
@@ -1,7 +1,15 @@
 function(klee_add_component target_name)
   add_library(${target_name} ${ARGN})
   # Use of `PUBLIC` means these will propagate to targets that use this component.
-  target_compile_options(${target_name} PUBLIC ${KLEE_COMPONENT_CXX_FLAGS})
+  if (("${CMAKE_VERSION}" VERSION_EQUAL "3.3") OR ("${CMAKE_VERSION}" VERSION_GREATER "3.3"))
+    # In newer CMakes we can make sure that the flags are only used when compiling C++
+    target_compile_options(${target_name} PUBLIC
+      $<$<COMPILE_LANGUAGE:CXX>:${KLEE_COMPONENT_CXX_FLAGS}>)
+  else()
+    # For older CMakes just live with the warnings we get for passing C++ only flags
+    # to the C compiler.
+    target_compile_options(${target_name} PUBLIC ${KLEE_COMPONENT_CXX_FLAGS})
+  endif()
   target_include_directories(${target_name} PUBLIC ${KLEE_COMPONENT_EXTRA_INCLUDE_DIRS})
   target_compile_definitions(${target_name} PUBLIC ${KLEE_COMPONENT_CXX_DEFINES})
 endfunction()
